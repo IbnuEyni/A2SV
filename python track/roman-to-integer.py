a3f1241be0463c611c46044d4e80ab8c6f13d7ca
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2023-12-01 17:24:53 +0300

    Added solution for Roman to Integer

---
+++ python track/roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mapp = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        res = 0
+        for i in range(len(s)):
+            if i < len(s) -1 and mapp[s[i]] < mapp[s[i+1]]:
+                res -= mapp[s[i]]
+            else:
+                res += mapp[s[i]]
+        return res
