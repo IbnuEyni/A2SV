0c3b44ef6dc607996a4abfe3d63691d1f5584b8d
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2023-12-06 18:42:32 +0300

    Added solution for Calculate Money in Leetcode Bank

---
+++ python track/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def totalMoney(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        no_week = n // 7
+        remain_day = n % 7
+
+        total = ((no_week * (no_week - 1)) // 2) * 7
+        total += no_week * 28
+        total += ((remain_day * (remain_day + 1)) // 2) + (no_week * remain_day)
+
+        return total
+       
