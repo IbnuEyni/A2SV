82040acd28811ff1abae41253e742c651c3ed976
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2023-12-19 22:05:01 +0300

    Added solution for Subdomain Visit Count

---
+++ leet-code-solutions/subdomain-visit-count.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        domain_count = defaultdict(int)
+        
+        for cpdomain in cpdomains:
+            count, domain = cpdomain.split(" ")
+            count = int(count)
+            
+            subdomains = domain.split(".")          
+            for i in range(len(subdomains)):
+                subdomain = ".".join(subdomains[i:])
+                domain_count[subdomain] += count
+
+        result = [str(count) + " " + subdomain for subdomain, count in domain_count.items()]
+        
+        return result
+
+        
