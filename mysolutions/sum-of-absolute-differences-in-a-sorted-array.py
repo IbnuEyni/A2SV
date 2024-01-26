a19d117a32c7c33ba89d7878e79ec76dfd6bab42
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2024-01-27 00:04:25 +0300

    Added solution for Sum of Absolute Differences in a Sorted Array

--- mysolutions/sum-of-absolute-differences-in-a-sorted-array.py
+++ mysolutions/sum-of-absolute-differences-in-a-sorted-array.py
@@ -4,23 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # sum_ = sum(nums)
-        # res = []
-        # for i in range(len(nums)):
-        #     # summ = 0
-        #     # for j in range(len(nums)):
-        #     #     summ += abs(nums[i]-nums[j])
-        #     # res.append(summ)
-        #     pre = sum(nums[:i])
-        #     suf = sum(nums[i+1:])
-
-        #     temp = (nums[i] * i - pre) + (suf - nums[i] * (len(nums)-i-1))
-        #     res.append(temp)
-
-        # return res
-
-        sm,pr=sum(nums),0
+        sum_ = sum(nums)
+        pre = 0
+        res = []
         for i in range(len(nums)):
-            pr+=nums[i]
-            nums[i]=(((i+1)*nums[i]-pr)+(sm-pr)-(len(nums)-i-1)*nums[i])
-        return nums
+            # summ = 0
+            # for j in range(len(nums)):
+            #     summ += abs(nums[i]-nums[j])
+            # res.append(summ)
+            pre += nums[i]
+            suf = sum_ - pre
+
+            temp = (nums[i] * i - (pre-nums[i])) + (suf - nums[i] * (len(nums)-i-1))
+            res.append(temp)
+
+        return res
