4a35ac301428fcb72bc5d0348f9f6f9988bf0932
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2024-02-12 14:22:13 +0300

    Add solution for Linked List Cycle

--- mySolutions/leetcode/linked-list-cycle.py
+++ mySolutions/leetcode/linked-list-cycle.py
@@ -12,14 +12,13 @@ class Solution(object):
         """
         if not head or not head.next:
             return False
-        
+
         slow = head
         fast = head.next
 
         while slow != fast:
             if not fast or not fast.next:
                 return False
-            slow = slow.next
             fast = fast.next.next
-        
+            slow = slow.next
         return True
